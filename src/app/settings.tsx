1684ae2b40692a764515f94168896db293c5e846
akang05 <alvinkang758@gmail.com>
2026-04-23 16:27:37 -0400

    Add comments for settings functionality

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,1 +1,8 @@
+/* Light and Dark mode */
 
+/* Account info */
+  /* Name, email, password, date created, delete account*/
+
+/* Change email and password */
+  /* change/set button*/
+/* Notifications on/off */
